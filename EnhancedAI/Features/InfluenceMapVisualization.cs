8f284897c03afc615d25dc6a047731dead45f7b7
Michael P. Starkweather <mpstark@gmail.com>
2019-05-15 17:35:33 -0500

    Working tooltip for the influence map with compare

--- EnhancedAI/Features/InfluenceMapVisualization.cs
+++ EnhancedAI/Features/InfluenceMapVisualization.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BattleTech;
 using BattleTech.Rendering.UI;
 using BattleTech.UI;
+using BattleTech.UI.Tooltips;
+using HBS;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -27,44 +30,45 @@ namespace EnhancedAI.Features
             Hide();
 
             var map = unit.BehaviorTree.influenceMapEvaluator;
-            var hexToPos = new Dictionary<MapTerrainDataCell, Vector3>();
-            var hexToMax = new Dictionary<MapTerrainDataCell, float>();
+
+            var lowest = float.MaxValue;
             var highest = float.MinValue;
+            var total = 0f;
+            var hexToHighestData = new Dictionary<MapTerrainDataCell, HexFactorData>();
 
             for (var i = 0; i < map.firstFreeWorkspaceEvaluationEntryIndex; i++)
             {
                 var entry = map.WorkspaceEvaluationEntries[i];
-                var value = entry.GetHighestAccumulator();
                 var hex = unit.Combat.MapMetaData.GetCellAt(entry.Position);
-
-                hexToPos[hex] = entry.Position;
-
-                // find the highest at hex
-                if (hexToMax.ContainsKey(hex))
-                    hexToMax[hex] = Mathf.Max(hexToMax[hex], value);
-                else
-                    hexToMax.Add(hex, value);
+                var value = entry.GetHighestAccumulator();
 
                 highest = Mathf.Max(highest, value);
+                lowest = Mathf.Min(lowest, value);
+                total += value;
+
+                if (hexToHighestData.ContainsKey(hex) && hexToHighestData[hex].Value > value)
+                    continue;
+
+                var data = new HexFactorData
+                {
+                    FactorRecords = entry.ValuesByFactorName,
+                    MoveType = entry.GetBestMoveType(),
+                    Position = entry.Position,
+                    Value = value
+                };
+
+                hexToHighestData[hex] = data;
             }
 
-            var lowest = hexToMax.Values.Min();
-            var average = hexToMax.Values.Average();
-
-            foreach (var kvp in hexToMax)
+            var average = total / map.firstFreeWorkspaceEvaluationEntryIndex;
+            foreach (var hexData in hexToHighestData.Values)
             {
-                var hex = kvp.Key;
-                var value = kvp.Value;
-
                 var darkGrey = new Color(.15f, .15f, .15f);
-                Color color;
-                color = value <= average
-                    ? Color.Lerp(Color.red, darkGrey, (value - lowest) / (average - lowest))
-                    : Color.Lerp(darkGrey, Color.cyan, (value - average) / (highest - average));
+                var color = hexData.Value <= average
+                    ? Color.Lerp(Color.red, darkGrey, (hexData.Value - lowest) / (average - lowest))
+                    : Color.Lerp(darkGrey, Color.cyan, (hexData.Value - average) / (highest - average));
 
-                //color = Color.Lerp(Color.black, Color.white, (value - lowest) / (highest - lowest));
-
-                ShowDotAt(hexToPos[hex], color);
+                ShowDotAt(hexData.Position, color, hexData);
             }
         }
 
@@ -76,10 +80,12 @@ namespace EnhancedAI.Features
             _unusedDotPool.AddRange(_usedDotPool);
             _usedDotPool.Clear();
             _parent.SetActive(false);
+
+            DotTooltip.CompareAgainst = null;
         }
 
 
-        private static void ShowDotAt(Vector3 location, Color color)
+        private static void ShowDotAt(Vector3 location, Color color, HexFactorData factorData)
         {
             GameObject dot;
             if (_unusedDotPool.Count > 0)
@@ -103,12 +109,22 @@ namespace EnhancedAI.Features
                 meshRenderer.receiveShadows = false;
                 meshRenderer.shadowCastingMode = ShadowCastingMode.Off;
 
+                var collider = dot.AddComponent<CapsuleCollider>();
+                collider.center = Vector3.zero;
+                collider.radius = 4f;
+                collider.height = 1f;
+                collider.isTrigger = true;
+
+                dot.AddComponent<DotTooltip>();
                 dot.AddComponent<UISweep>();
             }
 
             _usedDotPool.Add(dot);
             dot.transform.position = location + _groundOffset;
             dot.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+
+            var tooltip = dot.GetComponent<DotTooltip>();
+            tooltip.FactorData = factorData;
 
             var renderer = dot.GetComponent<MeshRenderer>();
             renderer.material.color = color;
@@ -144,6 +160,82 @@ namespace EnhancedAI.Features
             mesh.triangles = triangleList.ToArray();
 
             return mesh;
+        }
+    }
+
+    public class HexFactorData
+    {
+        public Vector3 Position;
+        public float Value;
+        public MoveType MoveType;
+        public Dictionary<string, EvaluationDebugLogRecord> FactorRecords;
+    }
+
+    public class DotTooltip : MonoBehaviour
+    {
+        public static HexFactorData CompareAgainst;
+        public HexFactorData FactorData;
+        private readonly HBSTooltipStateData _tooltipStateData = new HBSTooltipStateData();
+
+
+        public void OnMouseEnter()
+        {
+            _tooltipStateData.SetString(GetTooltipString());
+            LazySingletonBehavior<TooltipManager>.Instance.SpawnTooltip(
+                _tooltipStateData.GetTooltipObject(), 792402, 792402,
+                0f, _tooltipStateData.GetPrefabOverride(), true);
+        }
+
+        public void OnMouseExit()
+        {
+            LazySingletonBehavior<TooltipManager>.Instance.ClearTooltip();
+        }
+
+        public void OnMouseDown()
+        {
+            if (CompareAgainst == FactorData)
+                CompareAgainst = null;
+            else
+                CompareAgainst = FactorData;
+        }
+
+
+        private string GetTooltipString()
+        {
+            var factors = new Dictionary<string, float>();
+            foreach (var recordKVP in FactorData.FactorRecords)
+                factors.Add(recordKVP.Key, GetEntryValue(recordKVP.Value, FactorData.MoveType));
+
+            var value = FactorData.Value;
+            if (CompareAgainst != null)
+            {
+                foreach (var recordKVP in CompareAgainst.FactorRecords)
+                    factors[recordKVP.Key] -= GetEntryValue(recordKVP.Value, CompareAgainst.MoveType);
+
+                value -= CompareAgainst.Value;
+            }
+            var tooltip = $"\t{value:0.00} [{Enum.GetName(typeof(MoveType), FactorData.MoveType)}]";
+
+            if (CompareAgainst != null)
+                tooltip += " COMPARISON";
+
+            var sortedNames = factors.Keys.ToList();
+            sortedNames.Sort((one, two) =>
+                Mathf.Abs(factors[one]).CompareTo(Mathf.Abs(factors[two])));
+            sortedNames.Reverse();
+
+            foreach (var factorName in sortedNames)
+            {
+                if (Math.Abs(factors[factorName]) > 0.01)
+                    tooltip += $"\n\t{factors[factorName]:0.00} : {factorName}";
+            }
+
+            return tooltip;
+        }
+
+        private static float GetEntryValue(EvaluationDebugLogRecord record, MoveType moveType)
+        {
+            return moveType == MoveType.Sprinting ? record.SprintValue : record.RegularValue;
         }
     }
 }
