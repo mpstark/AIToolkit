7b265567ab75c4e17826dbe0540103a1e8ba3346
Michael P. Starkweather <mpstark@gmail.com>
2019-05-16 11:40:03 -0500

    Skip InfluenceMapEvaluator.ValidateCoverage because it's unnessary and spams logs if a factor is removed

---
+++ EnhancedAI/Patches/InfluenceMapEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using BattleTech;
+using Harmony;
+
+// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+
+namespace EnhancedAI.Patches
+{
+    /// <summary>
+    /// Skip non-essential ValidateCoverage method, as it spams log if it detects
+    /// that one of the enum values isn't hooked up
+    /// </summary>
+    [HarmonyPatch(typeof(InfluenceMapEvaluator), "ValidateCoverage")]
+    public static class InfluenceMapEvaluator_ValidateCoverage_Patch
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
